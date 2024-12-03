4b9ce40df55f882a0a4464f9fbe48771e030db6d
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-12-03 13:57:49 -0500

    black formatting

--- ITER_scenario.py
+++ ITER_scenario.py
@@ -55,10 +55,12 @@ risp1_longer_wait = Pulse(
     ramp_up=10,
     steady_state=250,
     ramp_down=10,
-    waiting=242730, #risp1 waiting plus two days nothing
+    waiting=242730,  # risp1 waiting plus two days nothing
     tritium_fraction=0.0,
 )
-benchmark_no_glow_scenario = Scenario(pulses=[fp,icwc,risp5,risp1,icwc,risp5,risp1_longer_wait])
+benchmark_no_glow_scenario = Scenario(
+    pulses=[fp, icwc, risp5, risp1, icwc, risp5, risp1_longer_wait]
+)
 
 ############## DO NOTHING SCENARIO ##############
 fp_do_nothing = Pulse(
@@ -67,7 +69,7 @@ fp_do_nothing = Pulse(
     ramp_up=429,
     steady_state=650,
     ramp_down=455,
-    waiting=430466, # fp waiting time plus 4 days of nothing
+    waiting=430466,  # fp waiting time plus 4 days of nothing
     tritium_fraction=0.5,
 )
 do_nothing_scenario = Scenario(pulses=[fp_do_nothing])
@@ -97,10 +99,12 @@ icwc_long = Pulse(
     ramp_up=50,
     steady_state=1200,
     ramp_down=50,
-    waiting=74300, # icwc waiting plus rest of day
+    waiting=74300,  # icwc waiting plus rest of day
     tritium_fraction=0.0,
 )
-clean_every_5_scenario = Scenario(pulses=[fp,risp6,icwc_long,fp,risp6,icwc_long,gdc])
+clean_every_5_scenario = Scenario(
+    pulses=[fp, risp6, icwc_long, fp, risp6, icwc_long, gdc]
+)
 
 ############## CLEAN EVERY 2 DAYS SCENARIO ##############
 fp = Pulse(
@@ -125,9 +129,9 @@ icwc_short = Pulse(
     pulse_type="ICWC",
     nb_pulses=1,
     ramp_up=50,
-    steady_state=500, # half the time of regular icwc
+    steady_state=500,  # half the time of regular icwc
     ramp_down=50,
-    waiting=30000, # icwc waiting takes us to 16h for FP pulse 
+    waiting=30000,  # icwc waiting takes us to 16h for FP pulse
     tritium_fraction=0.0,
 )
 fp1_start_day = Pulse(
@@ -153,9 +157,30 @@ icwc_short_end_day = Pulse(
     pulse_type="ICWC",
     nb_pulses=1,
     ramp_up=50,
-    steady_state=500, # half the time of regular icwc
+    steady_state=500,  # half the time of regular icwc
     ramp_down=50,
-    waiting=73200, # icwc waiting takes us to next day 
+    waiting=73200,  # icwc waiting takes us to next day
     tritium_fraction=0.0,
 )
-clean_every_2_scenario = Scenario(pulses=[fp,risp3,icwc_short,fp1_end_day,fp1_start_day,risp3,icwc_short_end_day, fp, risp3, icwc_short, fp1_end_day, fp1_start_day, risp3, icwc_short_end_day, fp, risp6, icwc_long,gdc])
+clean_every_2_scenario = Scenario(
+    pulses=[
+        fp,
+        risp3,
+        icwc_short,
+        fp1_end_day,
+        fp1_start_day,
+        risp3,
+        icwc_short_end_day,
+        fp,
+        risp3,
+        icwc_short,
+        fp1_end_day,
+        fp1_start_day,
+        risp3,
+        icwc_short_end_day,
+        fp,
+        risp6,
+        icwc_long,
+        gdc,
+    ]
+)
