4d215fb0b6d43f7cc519ce221c84c7148147b9bf
kaelyndunnell <kaelyndunnell03@gmail.com>
2024-12-19 22:46:48 -0800

    manual test to ensure all scenarios are exactly 14 days long

---
+++ test_scenario_times.py
@@ -0,0 +1,25 @@
+from iter_scenarios.benchmark import scenario as benchmark_scenario
+from iter_scenarios.clean_every_2_days import scenario as clean_every_2_scenario
+from iter_scenarios.clean_every_5_days import scenario as clean_every_5_scenario
+from iter_scenarios.no_glow import scenario as no_glow_scenario
+from iter_scenarios.do_nothing import scenario as do_nothing_scenario
+
+from hisp.scenario import Pulse
+
+def test_scenario_times(scenario_list):
+    """Tests that all scenarios are 14 days exactly.
+    """
+    expected_scenario_total_time = 14*24*3600 # 14 days in seconds
+
+    for scenario in scenario_list: 
+        total_time = 0.0
+        for pulse in scenario.pulses: 
+            total_time += pulse.total_duration * pulse.nb_pulses
+    
+        assert expected_scenario_total_time == total_time
+
+    print("All tests have passed!")
+
+if __name__=="__main__": 
+    my_scenarios = [benchmark_scenario, clean_every_2_scenario, clean_every_5_scenario, no_glow_scenario, do_nothing_scenario]
+    test_scenario_times(my_scenarios)
