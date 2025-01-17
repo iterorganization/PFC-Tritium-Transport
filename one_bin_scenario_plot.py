b20699ec7508307d2e7b81b32e4f99aa67684827
Rémi Delaporte-Mathurin <40028739+RemDelaporteMathurin@users.noreply.github.com>
2025-01-17 15:55:31 +0000

    fixed plotting for sub-bins

--- one_bin_scenario_plot.py
+++ one_bin_scenario_plot.py
@@ -9,7 +9,7 @@ TRITIUM_AMU = 3.016049  # g/mol
 D_AMU = 2.014102  # g/mol
 AVOGADROS_CONST = 6.0221408e23  # atms/mol
 
-BIN_INDEX = 18
+BIN_INDEX = 0
 
 if BIN_INDEX in range(total_fw_bins):
     my_bin = FW_bins.get_bin(BIN_INDEX)
@@ -20,7 +20,7 @@ avg_r_coord = 0.5 * abs(my_bin.start_point[0] + my_bin.end_point[0])
 bin_surf_area = 2 * math.pi * avg_r_coord * my_bin.length
 
 # open results file
-with open("processed_data.json", "r") as file:
+with open("results_benchmark.json", "r") as file:
     dict_data = json.load(file)
 
 
@@ -41,13 +41,12 @@ bin_data = get_bin_data(dict_data, BIN_INDEX)
 
 if BIN_INDEX in list(range(18,65)):
     plt.figure()
-    for i in bin_data.items():
-        name = i[0]
+    for name, value in bin_data.items():
         # skip the bin_index
         if name == "bin_index":
             continue
         print(name)
-        quantities_dict = i[1]
+        quantities_dict = value
         data = np.array(quantities_dict["data"])
         if "D" in name:
             plt.plot(
@@ -72,6 +71,7 @@ if BIN_INDEX in list(range(18,65)):
     plt.title("Bin " + str(BIN_INDEX+1) + " Results Benchmark Scenario")
     plt.yscale("log")
     plt.legend()
+    plt.savefig('out.png')
     plt.show()
 
 else:
@@ -79,15 +79,13 @@ else:
     for sub_bin in sub_bin_data:
         mode = sub_bin["mode"]
         plt.figure()
-        for i in sub_bin.items():
-            name = i[0]
+        for name, quantities_dict in sub_bin.items():
             # skip the parent bin_index
             if name == "parent_bin_index":
                 continue
             if name == "mode": 
                 continue
             print(name)
-            quantities_dict = i[1]
             data = np.array(quantities_dict["data"])
             if "D" in name:
                 plt.plot(
@@ -112,6 +110,7 @@ else:
         plt.title("Bin " + str(BIN_INDEX+1) + " "+mode+" Results Benchmark Scenario")
         plt.yscale("log")
         plt.legend()
+        plt.savefig(f'bin_{BIN_INDEX+1}_{mode}.png')
         plt.show()
 
 
