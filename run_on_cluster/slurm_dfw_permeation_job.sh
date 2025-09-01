15728b2f20f34c604400c2ed8c7f710375f1953d
wautert <tom.wauters@iter.org>
2025-09-01 15:00:45 +0200

    Updated readme and job script

--- run_on_cluster/slurm_dfw_permeation_job.sh
+++ run_on_cluster/slurm_dfw_permeation_job.sh
@@ -8,27 +8,6 @@
 #SBATCH --mem=1gb                 # Adjust memory
 #SBATCH --partition=sirius       # Adjust partition name
 
-# Load modules (if required)
-module load IMAS
-# module load FESTIM
-
-# Activate virtual environment
-source myenv/bin/activate
-
-# Load other modules
-ml foss
-ml SciPy-bundle 
-ml mpi4py
-ml tqdm
-ml scifem
-ml dolfinx/0.9.0-foss-2023b
-
-# Install correct FESTIM version
-python -m pip install --ignore-installed git+https://github.com/festim-dev/FESTIM@d1b71deed2d0998159b99591951493bffa1f5ca8
-
-# Install correct HISP version 
-python -m pip install git+https://github.com/kaelyndunnell/hisp@dfw-permeation-model
-
 # Loop over bins and modes
 for i in 13 14; do  # First wall bins
     modes=('shadowed')
@@ -48,16 +27,10 @@ for i in 13 14; do  # First wall bins
 
 # Load modules and activate environment
 module load IMAS
+module load Python
 source myenv/bin/activate
-ml foss
-ml SciPy-bundle 
-ml mpi4py
-ml tqdm
-ml scifem
-ml dolfinx/0.9.0-foss-2023b
 
 # Run the Python script
-# INPUT SCENARIO YOU'D LIKE TO TEST
 python run_on_cluster/run_dfw_bin.py $i $mode iter_scenarios benchmark 
 EOF
     done
