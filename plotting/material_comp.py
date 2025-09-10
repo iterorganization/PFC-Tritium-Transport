7b1fe816105ca88b9364166d5f7f0b15c764e052
Dunnell Kaelyn EXT <Kaelyn.Dunnell@iter.org>
2025-09-10 16:38:05 +0200

    update plottign func

--- plotting/material_comp.py
+++ plotting/material_comp.py
@@ -3,6 +3,9 @@ import plotly.express as px
 from make_iter_bins import Div_bins, total_fw_bins, FW_bins
 from plotly.subplots import make_subplots
 import json
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 
 # import scenarios
@@ -66,7 +69,9 @@ for name, value in t_plot_data.items():
 # ss_inv = np.insert(ss_inv, 0, 0)
 
 # create scatter plot 
-fig = make_subplots(rows=1, cols=2, shared_xaxes=False, subplot_titles=("Wall Inventory", "Divertor Inventory"))
+#create scatter plot in matplotlib
+fig, (ax_left, ax_right) = plt.subplots(1, 2)
+
 
 # pull milestones for plotting on time 
 time_points = [0]
@@ -108,84 +113,120 @@ plot_time = np.array(time_points) / 3600
 #     )
 # )
 
-fig.add_trace(
-    go.Scatter(
-        x=["After FP10", "Afer BAKE"],
-        y=boron_inv_wall,
-        # mode='markers',
-        name="Boron Inventory",
-        line=dict(color="firebrick", width=2, dash='dash'),
-    ),
-    row=1, col=1
-)
+ax_left.plot([0, 1], boron_inv_wall, label="Boron Inventory", color="firebrick",linestyle="dashed", marker="o", linewidth=2)
+ax_left.plot([0, 1], tungsten_inv_wall, label="Tungsten Inventory", color="royalblue",linestyle="dashed", marker="o", linewidth=2)
+ax_left.annotate(text="B", xy=(1 + 0.1, boron_inv_wall[-1] + 0.1), color="firebrick")
+ax_left.annotate(text="W", xy=(1 + 0.1, tungsten_inv_wall[-1] + 0.1), color="royalblue")
+ax_left.annotate(f"-{float(np.diff(boron_inv_wall))/boron_inv_wall[0]:.2%}", xy=(0.5, np.mean(boron_inv_wall)*1.1) , color="firebrick")
+ax_left.annotate(f"-{float(np.diff(tungsten_inv_wall))/tungsten_inv_wall[0]:.2%}", xy=(0.5, np.mean(tungsten_inv_wall)*1.01) , color="royalblue")
+# fig.add_trace(
+#     go.Scatter(
+#         x=["After FP10", "Afer BAKE"],
+#         y=boron_inv_wall,
+#         # mode='markers',
+#         name="Boron Inventory",
+#         line=dict(color="firebrick", width=2, dash='dash'),
+#     ),
+#     row=1, col=1
+# )
 
-fig.add_trace(
-    go.Scatter(
-        x=["After FP10", "Afer BAKE"],
-        y=tungsten_inv_wall,
-        # mode='markers',
-        name="Tungsten Inventory",
-        line=dict(color="royalblue", width=2, dash='dash'),
-    ),
-    row=1, col=1
-)
+# fig.add_trace(
+#     go.Scatter(
+#         x=["After FP10", "Afer BAKE"],
+#         y=tungsten_inv_wall,
+#         # mode='markers',
+#         name="Tungsten Inventory",
+#         line=dict(color="royalblue", width=2, dash='dash'),
+#     ),
+#     row=1, col=1
+# )
 
-fig.add_trace(
-    go.Scatter(
-        x=["After FP10", "Afer BAKE"],
-        y=ss_inv_wall,
-        name="SS316 Inventory",
-        # mode='markers',
-        line=dict(color="forestgreen", width=2, dash='dash'),
-    ),
-    row=1, col=1
-)
+# fig.add_trace(
+#     go.Scatter(
+#         x=["After FP10", "Afer BAKE"],
+#         y=ss_inv_wall,
+#         name="SS316 Inventory",
+#         # mode='markers',
+#         line=dict(color="forestgreen", width=2, dash='dash'),
+#     ),
+#     row=1, col=1
+# )
 
-fig.add_trace(
-    go.Scatter(
-        x=["After FP10", "Afer BAKE"],
-        y=boron_inv_div,
-        # mode='markers',
-        name="Boron Inventory",
-        line=dict(color="firebrick", width=2, dash='dash'),
-        showlegend=False
-    ),
-    row=1, col=2
-)
+ax_right.plot([0, 1], boron_inv_div, marker="o",color="firebrick",linestyle="dashed", linewidth=2)
+ax_right.plot([0, 1], tungsten_inv_div, marker="o",color="royalblue",linestyle="dashed", linewidth=2)
 
-fig.add_trace(
-    go.Scatter(
-        x=["After FP10", "Afer BAKE"],
-        y=tungsten_inv_div,
-        # mode='markers',
-        name="Tungsten Inventory",
-        line=dict(color="royalblue", width=2, dash='dash'),
-        showlegend=False
-    ),
-    row=1, col=2
-)
+ax_right.annotate(text="B", xy=(1.1, boron_inv_div[-1] + 0.1), color="firebrick")
+ax_right.annotate(text="W", xy=(1.1, tungsten_inv_div[-1] + 0.1), color="royalblue")
 
-fig.add_trace(
-    go.Scatter(
-        x=["After FP10", "Afer BAKE"],
-        y=ss_inv_div,
-        name="SS316 Inventory",
-        # mode='markers',
-        line=dict(color="forestgreen", width=2, dash='dash'),
-        showlegend=False
-    ),
-    row=1, col=2
-)
+# ax_right.annotate(f"x{boron_inv_div[-1]/boron_inv_div[0]:.2f}", xy=(0.5, np.mean(boron_inv_div)*1.1) , color="firebrick")
+# ax_right.annotate(f"x{tungsten_inv_div[-1]/tungsten_inv_div[0]:.2f}", xy=(0.5, np.mean(tungsten_inv_div)*1.1) , color="royalblue")
 
+ax_right.annotate(f"-{float(np.diff(boron_inv_div))/boron_inv_div[0]:.2%}", xy=(0.5, np.mean(boron_inv_div)*1.1) , color="firebrick")
+ax_right.annotate(f"-{float(np.diff(tungsten_inv_div))/tungsten_inv_div[0]:.2%}", xy=(0.5, np.mean(tungsten_inv_div)*1.05) , color="royalblue")
+# fig.add_trace(
+#     go.Scatter(
+#         x=["After FP10", "Afer BAKE"],
+#         y=boron_inv_div,
+#         # mode='markers',
+#         name="Boron Inventory",
+#         line=dict(color="firebrick", width=2, dash='dash'),
+#         showlegend=False
+#     ),
+#     row=1, col=2
+# )
 
-fig.update_xaxes(title_text="Time in Scenario")
-fig.update_yaxes(title_text="Inventory (atms)", type='log', row=1, col=1)
-fig.update_yaxes(title_text="Inventory (atms/m)", type='log', row=1, col=2)
-fig.update_traces(marker=dict(size=25))
+# fig.add_trace(
+#     go.Scatter(
+#         x=["After FP10", "Afer BAKE"],
+#         y=tungsten_inv_div,
+#         # mode='markers',
+#         name="Tungsten Inventory",
+#         line=dict(color="royalblue", width=2, dash='dash'),
+#         showlegend=False
+#     ),
+#     row=1, col=2
+# )
 
-fig.update_layout(title_text="Total Inventory for after 10 FP Pulses",
-        legend=dict(title=dict(text="Material")))
+# fig.add_trace(
+#     go.Scatter(
+#         x=["After FP10", "Afer BAKE"],
+#         y=ss_inv_div,
+#         name="SS316 Inventory",
+#         # mode='markers',
+#         line=dict(color="forestgreen", width=2, dash='dash'),
+#         showlegend=False
+#     ),
+#     row=1, col=2
+# )
 
-fig.write_html("do_nothing_materials.html", auto_open=True)
+ax_left.set_yscale("log")
+ax_right.set_yscale("log")
+ax_left.set_ylabel("Inventory (T)")
+ax_right.set_ylabel("Inventory (T/m)")
+# ax_top.legend(loc='upper left')
+ax_left.grid(which="both", alpha=0.3)
+ax_right.grid(which="both", alpha=0.3)
 
-print('Finished.')
+ax_left.set_title('Wall')
+ax_right.set_title('Divertor')
+
+ax_left.set_xticks([0, 1], ["After FP10", "Afer BAKE"])
+ax_right.set_xticks([0, 1], ["After FP10", "Afer BAKE"])
+# --- Format bottom axis ---
+# ax_bottom.set_ylabel("Surface Temp")
+# ax_left.legend(loc='upper right')
+
+#legend 
+# tungsten=mpatches.Patch(color='royalblue',label="W")
+# boron=mpatches.Patch(color='firebrick',label="B")
+
+for ax in [ax_left, ax_right]:
+    ax.spines[['top', 'right']].set_visible(False)
+    ax.set_xlim(right=1.2)
+    # ax.set_ylim(1e18, 1e20)
+
+plt.tight_layout()
+# plt.legend(handles=[tungsten,boron],loc='upper right')
+plt.savefig("paper_plots/material-comp.pdf", bbox_inches="tight")
+plt.savefig("paper_plots/material-comp.png", bbox_inches="tight")
+print('done')
